75b4668d3b2ab88613c74c0ee23824a87ef0e70c
hdd-disk <coolkotik228@gmail.com>
2026-07-23 13:54:17 +0300

    feat: add monet themes
    Thanks to Nekogram and Telegram Monet

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -253,6 +253,8 @@ import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.justgram.messenger.helpers.MonetHelper;
+
 public class LaunchActivity extends BasePermissionsActivity implements INavigationLayout.INavigationLayoutDelegate, NotificationCenter.NotificationCenterDelegate, DialogsActivity.DialogsActivityDelegate, IPipActivity {
     public final static String EXTRA_FORCE_NOT_INTERNAL_APPS = "force_not_internal_apps";
     public final static String EXTRA_FORCE_REQUEST = "force_request";
@@ -703,6 +705,10 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         // Warn existing ntfy.sh users on first launch after this feature was added
         if (SharedConfig.isNtfyDefaultServer()) {
             NotificationsSettingsActivity.showNtfyDefaultServerDialog(this);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MonetHelper.registerReceiver(this);
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -6867,6 +6873,9 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         isActive = false;
         activeInstanceCount--;
         unregisterReceiver(batteryReceiver);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MonetHelper.unregisterReceiver(this);
+        }
 
         if (activeInstanceCount == 0) {
             onDestroyStaticResources();
