5d555d8b748dfcca19822a0490fab592c0b23de6
hdd-disk <coolkotik228@gmail.com>
2026-08-24 11:27:13 +0300

    feat: add option to disable bottom tabs subtitles

--- TMessagesProj/src/main/java/org/justgram/messenger/settings/JustgramAppearanceSettingsActivity.java
+++ TMessagesProj/src/main/java/org/justgram/messenger/settings/JustgramAppearanceSettingsActivity.java
@@ -1,24 +1,32 @@
 package org.justgram.messenger.settings;
 
+import static org.telegram.messenger.LocaleController.getString;
+
 import android.content.Context;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import org.justgram.messenger.JustgramConfig;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.R;
 import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.BaseFragment;
+import org.telegram.ui.ActionBar.INavigationLayout;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
 import org.telegram.ui.Components.UItem;
 import org.telegram.ui.Components.UniversalAdapter;
 import org.telegram.ui.Components.UniversalRecyclerView;
+import org.telegram.ui.LaunchActivity;
+import org.telegram.ui.MainTabsActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class JustgramAppearanceSettingsActivity extends BaseFragment {
-
     private UniversalRecyclerView listView;
+
+    private final static int ID_HIDE_BOTTOM_TABS_SUBTITLES = 1;
 
     @Override
     public View createView(Context context) {
@@ -39,7 +47,7 @@ public class JustgramAppearanceSettingsActivity extends BaseFragment {
         frameLayout.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
         fragmentView = frameLayout;
 
-        listView = new UniversalRecyclerView(this, this::fillItems, null, null);
+        listView = new UniversalRecyclerView(this, this::fillItems, (item, view, position, x, y) -> onClick(item, view), null);
         listView.setSections();
         frameLayout.addView(listView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
 
@@ -49,7 +57,30 @@ public class JustgramAppearanceSettingsActivity extends BaseFragment {
     }
 
     private void fillItems(ArrayList<UItem> items, UniversalAdapter adapter) {
-        items.add(UItem.asCenterShadow(LocaleController.getString(R.string.JustgramComingSoon)));
+        adapter.whiteSectionStart();
+        items.add(UItem.asCheck(ID_HIDE_BOTTOM_TABS_SUBTITLES, LocaleController.getString(R.string.HideBottomTabsSubtitles)).setChecked(JustgramConfig.hideTabsSubtitles));
+        adapter.whiteSectionEnd();
     }
 
+    private void onClick(UItem item, View view) {
+        switch (item.id) {
+            case ID_HIDE_BOTTOM_TABS_SUBTITLES:
+                JustgramConfig.hideTabsSubtitles = !JustgramConfig.hideTabsSubtitles;
+                JustgramConfig.saveConfig();
+                listView.adapter.update(true);
+
+                if (LaunchActivity.instance != null) {
+                    INavigationLayout navigationLayout = LaunchActivity.instance.getActionBarLayout();
+                    if (navigationLayout != null) {
+                        List<BaseFragment> fragments = navigationLayout.getFragmentStack();
+                        for (BaseFragment fragment : fragments) {
+                            if (fragment instanceof MainTabsActivity) {
+                                ((MainTabsActivity) fragment).updateBottomTabsLayout();
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
 }
